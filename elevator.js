2e79729f273969243d00d6227b995897dcac515a
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-03 09:34:19 -1000

    partil set up of the functions

--- elevator.js
+++ elevator.js
@@ -3,4 +3,48 @@
 
 module.exports = {
 
+  goto : function(floor, button){
+
+    return 'poop';
+  }
+
+  floorCheck :function(floor, button){
+
+    if(floor >= 0 && floor <=3){
+      goto(floor,button)
+    }else{
+      floor = 0;
+      goto(floor,button)
+    }
+
+  }
+
+  buttonPush : function(floor, button){
+
+    if(button >= 0 && button <=3){
+      goto(floor,button)
+    }else{
+      button = floor;
+      goto(floor,button)
+    }
+
+  }
+
+  mover : function(floor, button){
+    if(button > floor){
+      goto(floor,button);
+    }else if(floor > button){
+      button = button * -1;
+      goto(floor, button)
+    }else{
+      goto(floor,button);
+    }
+  }
+
+
+
+
+
+
+
 };
