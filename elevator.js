4fa515dcccfc1562139fee10990782d59118b7c2
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-03 13:32:03 -1000

    final solution with tests

--- elevator.js
+++ elevator.js
@@ -4,29 +4,33 @@
 module.exports = {
 
   goto : function(floor, button){
-    //Check the floor       //check the button
-    if(floor >= 0 && floor <=3 && button >= 0 && button <=3){
 
-      if(button > floor){
-        console.log('Welcome to floor ' + button);
-        return button;
+    if(typeof(floor) === 'number' && typeof(button) === 'string'){
 
-      }else if(floor > button){
-        button = button * -1;
-        console.log('Welcome to floor ' + (button * -1));
-        return (button * -1);
+      button = Number(button);
+      //Check the floor       //check the button
+      if(floor >= 0 && floor <= 3 && button >= 0 && button <= 3){
 
+        if(button > floor){
+          return button;
+
+        }else if(floor > button){
+          floor = (button - floor);
+          return (floor);
+
+        }else{
+          return button;
+        }
       }else{
-        console.log('You are still on floor ' + button);
-        return button;
+        return 0;
       }
-
     }
     //either floor input or button input is not 0,1,2,3
     else{
-      console.log('Floor not recognized. You have not gone anywhere!');
       return 0;
     }
+
+
 
   }
 }
